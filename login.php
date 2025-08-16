65aec84bd854458c74de3605b56fbe28f7ca1a91
Bright Munemu <brightmunemu@gmail.com>
2025-08-16 23:17:09 +0100

    add file

--- login.php
+++ login.php
@@ -80,7 +80,7 @@
 
   <!-- 1. HERO SECTION -->
   <header>
-    <h1>Welcome to ExamNow</h1>
+    <h1>Welcome to ExameNow</h1>
     <p>The smart way to prepare, practice, and excel in your Computer-Based Tests.</p>
   </header>
 
