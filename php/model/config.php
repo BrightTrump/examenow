eacb510e1cc4042a12ef99204e4680d31b2f4289
Bright Munemu <brightmunemu@gmail.com>
2025-09-22 23:28:20 +0200

    add details

--- php/model/config.php
+++ php/model/config.php
@@ -1,14 +1,14 @@
 <?php 
 session_start();
 class config{
+     public $host = "localhost";
+     public $user = "u787607796_examenow";
+     public $password = "Examenow1234@";
+     public $database = "u787607796_examenow";
     // public $host = "localhost";
-    // public $user = "u629877266_examenow";
-    // public $password = "Examenow1234@";
-    // public $database = "u629877266_examenow";
-    public $host = "localhost";
-    public $user = "root";
-    public $password = "";
-    public $database = "examenow";
+    // public $user = "root";
+    // public $password = "";
+    // public $database = "examenow";
 
     public $connection;
 
